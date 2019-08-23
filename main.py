d6365648b316af3d9d2ad9cb9987bdaf680f8197
Edison Lai <useful390@cstrail.com>
2019-08-23 10:04:07 -0700

    Init the repository;

---
+++ main.py
@@ -0,0 +1,25 @@
+import xlrd, xlwt, xlutils
+import os
+
+def write_excel (filename, data):
+    book = xlwt.Workbook()
+    sheet = book.add_sheet ('sheet1')
+    c = 0
+    for d in data:
+        for index in range(len(d)):
+            sheet.write(c, index, d[index])
+            c = c + 1
+            book.save (filename)
+    return
+
+def read_filename_in_folder (filepath, list):
+    for root, dirs, files in os.walk(filepath):
+        list.append(files)
+    return
+
+filename_list = []
+source = raw_input("输入读取的文件夹路径：")
+dest = raw_input("选择输出路径：")
+read_filename_in_folder(source, filename_list)
+write_excel(dest, filename_list)
+
